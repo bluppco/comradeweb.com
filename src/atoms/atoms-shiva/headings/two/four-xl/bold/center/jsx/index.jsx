99a7d84a1d45fdb78d008a7ee8e4417a5ca24baf
Shiva pandey <shivapandey9898@gmail.com>
2024-07-29 15:48:26 +0530

    feat: created atom for heading two

---
+++ src/atoms/atoms-shiva/headings/two/four-xl/bold/center/jsx/index.jsx
@@ -0,0 +1,12 @@
+const HeadingTwoFourXlBoldCenter = ( props ) => {
+
+    return (
+
+        <h2 class="text-3xl lg:text-4xl text-center font-montserrat font-bold">
+            { props.children }
+        </h2>
+
+    )
+
+}
+export default HeadingTwoFourXlBoldCenter
