640436d2234bc0c10fcd9a8dcd2f6509d7bc1cae
Shiva pandey <shivapandey9898@gmail.com>
2024-07-29 15:48:47 +0530

    feat: created atom for  paragraphs

---
+++ src/atoms/atoms-shiva/paragraphs/base/leading/jsx/index.jsx
@@ -0,0 +1,12 @@
+const ParagraphBaseLeading = ( props ) => {
+
+    return (
+
+        <p className="leading-8 font-pt_serif">
+            { props.children }
+        </p>
+
+    )
+
+}
+export default ParagraphBaseLeading
